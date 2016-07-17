417367e4d8fc9fea6db31bf49ac769e2eb9bd7d7
eprym <1198693608@qq.com>
2016-07-16 18:58:59 -0700

    Update WiggleSort2.java

--- src/WiggleSort2.java
+++ src/WiggleSort2.java
@@ -2,7 +2,7 @@ public class Solution {
     public void wiggleSort(int[] nums) {
        if(nums.length <= 1) return;
        Arrays.sort(nums);//maybe quick select here
-       int median = nums.length % 2 == 0 ? (nums[(nums.length-1)/2]+nums[(nums.length+1)/2])/2 : nums[nums.length/2];
+       int median = nums[nums.length/2];
        int first = 0, mid = 0, last = nums.length-1;
        int n = nums.length;
        while(mid <= last){
